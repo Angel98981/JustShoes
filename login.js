00eb9c71db62faf068ca773ffa0d43017090c395
Angel98981 <angel.rivera@udea.edu.co>
2024-01-17 17:17:00 -0500

    pagina principal y carro de compras

---
+++ login.js
@@ -0,0 +1,15 @@
+// const login =document.querySelector("#log");
+// const nombreUsuario="";
+// const datos = {nombre:"", email:""}
+
+// login.addEventListener("click",()=>{
+//     datos.nombre=document.querySelector("#name").value
+//     datos.email=document.querySelector("#email").value
+//     // const datosString = new URLSearchParams(datos).toString() 
+//     // window.location.href=`index.html?${datosString}`
+//     window.localStorage.setItem("dato", JSON.stringify(datos))
+//     window.location.href=`index.html`
+// });
+
+// const usuarioGuardado=JSON.parse(window.localStorage.getItem("dato"))
+// console.log(usuarioGuardado);

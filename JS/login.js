00a8dc31aa650e24dc05d2d4d8937f477a0b6606
Angel98981 <angel.rivera@udea.edu.co>
2024-01-18 07:09:07 -0500

    Carpetas organizadas
    de aqui toca que le den pull a las cosas para que se les actualice y trabajen con las carpetas, quiero decir que, cuando se referencia algo como un script o un style toca poner la nueva referencia ejemplo, voy a referenciar en el html del login la hoja de style pongo ../css/login.css de esta manera se referencia correctamente.

--- JS/login.js
+++ JS/login.js
@@ -13,3 +13,14 @@
 
 // const usuarioGuardado=JSON.parse(window.localStorage.getItem("dato"))
 // console.log(usuarioGuardado);
+
+const login=document.querySelector("#login")
+
+login.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    const user=document.querySelector("#user").value
+    const pass=document.querySelector("#pass").value
+    console.log(user, pass);
+})
+
+

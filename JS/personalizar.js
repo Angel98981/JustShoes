59afb8d49bad54dbd50f1e7e6950cad91f4af8ca
cxmi02 <149894510+cxmi02@users.noreply.github.com>
2024-02-01 16:37:42 -0500

    personalizar
    La parte ce Html esta termina, en el css se aplicaron algunas correccion y ya quedo la parte de cambio de vista de imagenes en js

--- JS/personalizar.js
+++ JS/personalizar.js
@@ -1,19 +1,68 @@
-const usernameElement = document.getElementById("nombreUsuario");
-let loggedInUserName = JSON.parse(localStorage.getItem("user"));
-usernameElement.textContent = loggedInUserName.name;
+// SELECTORES
 
-const logout = document.getElementById("logout")
-let cerrar = JSON.parse(localStorage.getItem("validate"))
-// 
-if (cerrar === true) {
-  logout.classList.remove("deactivate")
-  usernameElement.classList.remove("deactivate")
-}
-const cerrarSesion = document.querySelector("#closeSesion")
-cerrarSesion.addEventListener("click",()=>{
-  logout.classList.add("deactivate")
-  usernameElement.classList.add("deactivate")
-  localStorage.removeItem("user")
-  localStorage.removeItem("validate")  
+// Imagenes
+const imgPrincipal = document.querySelector(".imgPrincipal");
+const imagenes = document.querySelectorAll(".imagenes");
+
+
+
+// EVENTOS
+
+// Imagenes
+imagenes.forEach(imagen => {
+  imagen.addEventListener("click", intercambiar )
+});
+
+
+// FUNCIONES
+
+// Imagenes
+function intercambiar (){
+  const imgSeleccionada = document.querySelector(".imgSeleccionada");
+  imgSeleccionada.classList.remove("imgSeleccionada");
+  this.classList.add("imgSeleccionada");
+  imgPrincipal.src = this.src;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Cerra sesion
+// const usernameElement = document.getElementById("nombreUsuario");
+// let loggedInUserName = JSON.parse(localStorage.getItem("user"));
+// usernameElement.textContent = loggedInUserName.name;
+
+// const logout = document.getElementById("logout")
+// let cerrar = JSON.parse(localStorage.getItem("validate"))
+// // 
+// if (cerrar === true) {
+//   logout.classList.remove("deactivate")
+//   usernameElement.classList.remove("deactivate")
+// }
+// const cerrarSesion = document.querySelector("#closeSesion")
+// cerrarSesion.addEventListener("click",()=>{
+//   logout.classList.add("deactivate")
+//   usernameElement.classList.add("deactivate")
+//   localStorage.removeItem("user")
+//   localStorage.removeItem("validate")  
   
-})
+// })

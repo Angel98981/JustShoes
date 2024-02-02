b9aefac3eb1748db04f04d42206c28b0c0055512
cxmi02 <149894510+cxmi02@users.noreply.github.com>
2024-02-02 14:44:15 -0500

    js y json

--- JS/personalizar.js
+++ JS/personalizar.js
@@ -1,8 +1,13 @@
+//URLS
+const URLProductos = "http://localhost:3000/productos"
 // SELECTORES
 
 // Imagenes
 const imgPrincipal = document.querySelector(".imgPrincipal");
 const imagenes = document.querySelectorAll(".imagenes");
+const contenedorProductos = document.querySelector("#contenedorProductos");
+const categorias = document.querySelectorAll(".categoria");
+const generos = document.querySelectorAll(".genero");
 
 
 
@@ -11,6 +16,21 @@ const imagenes = document.querySelectorAll(".imagenes");
 // Imagenes
 imagenes.forEach(imagen => {
   imagen.addEventListener("click", intercambiar )
+});
+
+contenedorProductos.addEventListener("click", e =>{
+  e.preventDefault();
+  console.log(e.target.getAttribute("id"));
+  if (e.target.classList.contains("contenido-modal")){
+    selectProduct(e.target.getAttribute("id"))
+  }
+})
+categorias.forEach(categoria => {
+  categoria.addEventListener("click", seleccionCategoria)
+});
+
+generos.forEach(genero => {
+  genero.addEventListener("click", seleccionGenero)
 });
 
 
@@ -24,6 +44,85 @@ function intercambiar (){
   imgPrincipal.src = this.src;
 };
 
+async function cargarProductos(){
+  const response = await fetch(URLProductos)
+  const productosElegidos = await response.json()
+  limpiar()
+  productosElegidos.forEach(productoElegido => {
+      console.log(productoElegido.id);
+      const div = document.createElement("div");
+      div.classList.add("contenido-modal");
+      div.setAttribute("id",`${productoElegido.id}`)
+      div.innerHTML += `
+      <img src="${productoElegido.imagenes.frontal}" alt="">
+      <h5 class="titulo-contenido">${productoElegido.titulo}</h5>
+      <p class="precio-contenido">${productoElegido.precio}</p>
+      `
+      contenedorProductos.appendChild(div);
+  });
+
+}
+
+cargarProductos();
+
+
+async function seleccionCategoria(e){
+  const response = await fetch(`${URLProductos}`);
+  const data = await response.json();
+  console.log(data);
+  // categorias.forEach(categoria => categoria.classList.remove("activo"));
+  // e.target.classList.add("active");
+
+  // const productosBtn = productos.filter(producto => producto.categoria.id === e.target.id)
+  // cargarProductos(productosBtn);
+
+  
+}
+
+
+function seleccionGenero(e){
+  generos.forEach(genero => genero.classList.remove("activo"));
+  e.target.classList.add("activo");
+  const generoBtn = productos.filter(producto => producto.genero.id === e.target.id)
+
+  const generoSeleccionado = e.target.id
+;
+  if(generoSeleccionado == "femenino"){
+  
+
+   }
+ if(generoSeleccionado == "masculino"){
+
+ }
+ cargarProductos(generoBtn)  
+  
+}
+
+async function selectProduct(id){
+  //Cerrar el modal
+  document.querySelector("#cerrar").click()
+  //Pintar pagina principal
+  const response = await fetch(`${URLProductos}/${id}`);
+  const data = await response.json();
+
+  document.querySelector(".Nombre").textContent = data.titulo;
+  document.querySelector(".precio").textContent = data.precio;
+  document.querySelector(".imgPrincipal").src = data.imagenes.frontal;
+  document.querySelector("#A").src = data.imagenes.frontal;
+  document.querySelector("#B").src = data.imagenes.trasera;
+  document.querySelector("#C").src = data.imagenes.izquierda;
+  document.querySelector("#D").src = data.imagenes.derecha;
+
+
+
+
+}
+
+function limpiar (){
+  while (contenedorProductos.firstChild) {
+      contenedorProductos.removeChild(contenedorProductos.firstChild)
+  }
+}
 
 
 
